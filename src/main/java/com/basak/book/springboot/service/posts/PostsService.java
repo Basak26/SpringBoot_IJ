b20803404aa55425c134234d24cc0918d430c993
Basak26 <yunje26@gmail.com>
2024-11-01 13:25:13 +0900

    게시글 등록, 수정, 삭제 기능 구현 완료

--- src/main/java/com/basak/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/basak/book/springboot/service/posts/PostsService.java
@@ -5,9 +5,12 @@ import com.basak.book.springboot.domain.posts.PostsRepository;
 import com.basak.book.springboot.web.dto.PostsResponseDto;
 import com.basak.book.springboot.web.dto.PostsSaveRequestDto;
 import com.basak.book.springboot.web.dto.PostsUpdateRequestDto;
+import com.basak.book.springboot.web.dto.PostsListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,16 +24,34 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
 
-    public PostsResponseDto findById (Long id) {
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/basak/book/springboot/web/IndexController.java
+++ src/main/java/com/basak/book/springboot/web/IndexController.java
@@ -1,21 +1,39 @@
 package com.basak.book.springboot.web;
 
+import com.basak.book.springboot.domain.posts.Posts;
+import com.basak.book.springboot.web.dto.PostsListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import com.basak.book.springboot.service.posts.PostsService;
+import com.basak.book.springboot.web.dto.PostsResponseDto;
 
 @RequiredArgsConstructor
 @Controller
 
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+    private final PostsService postsService;
+
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "Posts-save";
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
